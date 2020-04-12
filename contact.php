9e7bf59b5eaee8431861d17e326d24693e2e3c04
KartinL <kartin_28@hotmail.com>
2020-04-13 03:11:20 +1000

    changes to contact form

--- contact.php
+++ contact.php
@@ -133,13 +133,13 @@ http://www.tooplate.com/view/2082-pure-mix
 				<form id="contact-form" method="post" action="mailer-new.php">
 					<div class="row">
                         <?php
-                          if($_GET['success'] == 1) {
-                              echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>";
-                          }
-
-                          if($_GET['success'] == -1) {
-                              echo "<div class=\"form-messages error\">Oops! There seems to be an error. Please try again!</div>";
-                        }?>
+                        if($_GET['success'] == 1 ) {
+                            echo "<div class=\"form-messages success\">Thank you! Your message has been sent.</div>";
+                        }
+                        if ($_GET['success'] == -1 ) {
+                            echo "<div class=\"form-messages error\">Oops! There seems to be an error. Please try again.</div>";
+                        }
+                        ?>
                     </div>
 					<input name="name" type="text" class="form-control" placeholder="Your Name" required>
 					<input name="email" type="email" class="form-control" placeholder="Your Email" required>
